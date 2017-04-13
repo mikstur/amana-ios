3fe0d265a15adf486853ab548af3f7f847c3eee0
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2017-04-13 21:51:08 +0200

    Initial Commit

---
+++ amana/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  amana
+//
+//  Created by Miki von Ketelhodt on 2017/04/13.
+//  Copyright © 2017 RBG Applications. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ amana/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  amana
+//
+//  Created by Miki von Ketelhodt on 2017/04/13.
+//  Copyright © 2017 RBG Applications. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
